6ddc0b27128a997433abffbe489963274b74afba
CERobertson <CERobertson@gmail.com>
2016-07-26 23:20:26 -0700

    One of the variants of UDP recieving that did not work in Unity but work fine in .NET.  There are a couple adjacent solutions that don't work in Unity in their own unique way.  I may add them to this project for future reference.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UdpProof {
+    class Program {
+        static void Main(string[] args) {
+            var listener = new UdpListener();
+            listener.OnCommandRecieved += CommandReceieved;
+            Console.ReadLine();
+        }
+        static void CommandReceieved(string command) {
+            Console.WriteLine(command);
+        }
+    }
+
+    class UdpListener {
+
+        public delegate void CommandRecieved(string command);
+        public CommandRecieved OnCommandRecieved;
+        public int port = 52138;
+
+        Thread receiveThread;
+
+        public UdpListener() {
+            OnCommandRecieved += (data) => { return; };
+            receiveThread = new Thread(new ThreadStart(ReceiveData));
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+        private void ReceiveData() {
+            while (receiveThread.IsAlive) {
+                UdpClient receivingUdpClient = new UdpClient(port);
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                OnCommandRecieved(Encoding.ASCII.GetString(receiveBytes));
+                receivingUdpClient.Close();
+            }
+        }
+    }
+}
